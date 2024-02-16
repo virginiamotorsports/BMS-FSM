7b3da99b1c5ce7a305fe93ed4daf7ff2e3bb0daf
miktdc <miktadcakmak@gmail.com>
2024-02-16 16:37:20 -0500

    Progress to the structure of FSM

--- src/FSM.cpp
+++ src/FSM.cpp
@@ -7,6 +7,12 @@ int communicationsOnOff = 0;
 int fault = 1;
 int readyToRun = 0;
 
+bool optimalValuesAchieved();
+
+int establishConnection(){
+    return 0;
+}
+
 
 void transition(FSM_STATE nextState) {
     state.presentState = nextState;
@@ -46,12 +52,59 @@ void normalOperations() {
         fault = 1; //Fault pin goes high until fault
 
         //Exit Routine
-
+        if(cellBalancingTimer()){
+            transition(CELL_BALANCE);
+        }
     }
+}
+
+void unexpectedFault(){
+    //Perform hardware reset on device
+
+    //Send diagnostic CAN msg
+
+    fault = 0; //fault pin = low;
+
+    //Exit Routine
+    //run the routine??
+    transition(INITIAL);
+}
+
+bool commEstablished(){
+    //Check if comms are established
+    return false;
+}
+
+void commFault(){
+    while(1){
+        if(commEstablished()){
+            break;
+        }
+    }
+    fault = 0;
+    transition(STARTUP);
+}
+
+void tempVoltageFault(){
+    //Send CAN msgs and error state
+    while(1){
+        if(optimalValuesAchieved()){
+            break;
+        }
+    }
+    transition(NORMALOP);
+}
+
+bool optimalValuesAchieved() {
+    return false;
 }
 
 void cellBalancing() {
 
+}
+
+bool cellBalancingTimer() {
+    return false;
 }
 
 int main() {
@@ -59,7 +112,10 @@ int main() {
     while (1) {
         switch (state.presentState) {
             case INITIAL:
+                initial();
                 break;
+            default:
         }
     }
 }
+
