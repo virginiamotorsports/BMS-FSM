d96058e4ff3c4ff649274f628d10edbd6b765816
miktdc <miktadcakmak@gmail.com>
2024-02-16 15:28:49 -0500

    Progress to the structure of FSM

--- src/FSM.cpp
+++ src/FSM.cpp
@@ -2,46 +2,63 @@
 
 #include "FSM.h"
 
+State state;
+int communicationsOnOff = 0;
+int fault = 1;
+int readyToRun = 0;
 
 
-int main(){
-    State state;
+void transition(FSM_STATE nextState) {
+    state.presentState = nextState;
+}
 
+void initial() {
+    //Run Boot commands
+
+    //Establish communications
+    if (successfull) {
+        communicationsOnOff = 1;
+    }
+
+    //Exit Routine
+    if (communicationsOnOff) {
+        fault = 0; //Pull fault to low
+        transition(STARTUP);
+    }
+}
+
+void startup() {
+    if(communicationsOnOff){
+        //Write registers to the device
+
+        //Exit Routine
+        transition(NORMALOP);
+    }
+}
+
+void normalOperations() {
+    if(readyToRun){
+        //Read voltages and temps
+
+        //Send data via CAN
+
+        fault = 1; //Fault pin goes high until fault
+
+        //Exit Routine
+
+    }
+}
+
+void cellBalancing() {
+
+}
+
+int main() {
     state.presentState = INITIAL;
-
-    while(1){
-        switch(state.presentState){
+    while (1) {
+        switch (state.presentState) {
             case INITIAL:
-
-                std::cout << "inside INITIAL" << std::endl;
-
                 break;
-
-            case STARTUP:
-
-                break;
-
-            case NORMALOP:
-
-                break;
-
-            case CELL_BALANCE:
-
-                break;
-
-            case FAULT_COMM:
-
-                break;
-
-            case FAULT_TMPVOLT:
-
-                break;
-
-            case FAULT_UNEXPECTED:
-
-                break;
-
-
         }
     }
 }
